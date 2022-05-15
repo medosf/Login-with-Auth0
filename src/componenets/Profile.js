049d57d045dc935555acd4e8d435ba70e8386b32
medosf <43324373+medosf@users.noreply.github.com>
2022-05-14 22:11:44 -0700

    clean up

--- src/componenets/Profile.js
+++ src/componenets/Profile.js
@@ -2,13 +2,13 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
-  return (
-    isAuthenticated && (
-      <div className="profile-info">
-        {user?.picture && <img src={user.picture} alt="user picture" />}
-        <h4>Welcome, {user.name}</h4>
-      </div>
-    )
+  return isAuthenticated ? (
+    <div className="profile-info">
+      {user?.picture && <img src={user.picture} alt="user picture" />}
+      <h4>Welcome, {user.name}</h4>
+    </div>
+  ) : (
+    <p>please login to see this page</p>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const domain = "dev-xqbxsjgu.us.auth0.com";
-const clientId = "73UNjJsw4tTsE79BAlssK4Yx4mjKb197";
-
 root.render(
   <React.StrictMode>
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
     >
       <App />
