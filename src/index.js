78b35dd6458e39ccd9bba5829ad4e7fec8dd6dd9
medosf <43324373+medosf@users.noreply.github.com>
2022-05-14 17:48:29 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
